c5e43872bbdc631dfbd9c7f7463ed752218696b3
Ari <trachten@bu.edu>
2017-03-19 21:18:41 -0400

    minor tweak

--- myBloom.cpp
+++ myBloom.cpp
@@ -3,10 +3,13 @@
 //
 
 #include "myBloom.h"
+#include <cstring>
+#include <iostream>
+#include <string>
 
-myBloom::myBloom(int mm) : Bloom(mm) { filter = new char[length]; }
+myBloom::myBloom(int mm) : Bloom(mm) { filter = new char[length]; memset(filter, '_', length);}
 
-myBloom::myBloom(std::string newFilter) : myBloom(newFilter.length()) { strcpy(filter,newFilter.c_str()); }
+myBloom::myBloom(std::string newFilter) : myBloom(newFilter.length()) { strncpy(filter,newFilter.c_str(),length); }
 
 void myBloom::insert(std::string item) { filter[count++] = item[0]; }
 
@@ -17,6 +20,6 @@ bool myBloom::exists(std::string item) {
     return false;
 }
 
-std::string myBloom::output() { return filter; }
+std::string myBloom::output() { return std::string(filter, length); }
 
 myBloom::~myBloom() { delete[] filter; }
